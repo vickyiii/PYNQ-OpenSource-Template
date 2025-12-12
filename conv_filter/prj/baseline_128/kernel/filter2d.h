29d9ff2f1286af000c927e1aaf8de720174d5c33
vickyiii <w.chen@amd.com>
2025-12-12 15:18:33 +0800

    update V1.1

--- conv_filter/prj/baseline_128/kernel/filter2d.h
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2024 Advanced Micro Devices, Inc
-//
-// SPDX-License-Identifier: MIT
-
-typedef int DTYPE;
-
-void filter2d_accel(DTYPE* img_in, DTYPE* kernel, DTYPE* img_out, int rows, int cols);
